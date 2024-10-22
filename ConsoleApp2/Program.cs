33ce56cc126938a0d3ada7ad6e5c4845ca1827d2
Leonam Flores Mergareno <leonam.mergareno@sanesul.ms.gov.br>
2024-10-22 15:53:58 -0400

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorFinal;
+            do 
+            {
+                int iniciaZero = 0, iniciaUm = 1, procimoValor = 0, valorDigitado;
+                List<int> valores = new List<int>();
+                Console.WriteLine("Digite o valor: \n");
+                valorDigitado = Convert.ToInt32(Console.ReadLine());
+
+                do
+                {
+                    procimoValor = iniciaZero + iniciaUm;
+                    iniciaZero = iniciaUm;
+                    iniciaUm = procimoValor;
+                    valores.Add(procimoValor);
+                } while (procimoValor < valorDigitado);
+
+                if (valores.Contains(valorDigitado))
+                {
+                    Console.WriteLine("\nvalor pertence a sequencia de Fibonacci\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nvalor não pertence a sequencia de Fibonacci\n");
+                }
+                Console.WriteLine("\nDigite 0 para fechar:\n");
+                valorFinal = Convert.ToInt32(Console.ReadLine());
+            } while (valorFinal != 0);
+        }
+    }
+}
---
+++ ConsoleApp2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ConsoleApp2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SANESUL")]
+[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyCopyright("Copyright © SANESUL 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a7b295b2-97eb-4b19-8afe-4f19e51353a4")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
